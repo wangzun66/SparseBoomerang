2ec3291a54d22ad44ea7fe2e59d8176862d8d929
zunhoho <wangzun666@gmail.com>
2024-07-22 09:02:58 +0200

    fix FeatureExtractor

--- boomerangPDS/src/main/java/boomerang/strategydecider/FeatureExtractor.java
+++ boomerangPDS/src/main/java/boomerang/strategydecider/FeatureExtractor.java
@@ -3,34 +3,32 @@ package boomerang.strategydecider;
 import core.fx.base.*;
 import core.fx.methodbased.AllocationCount;
 import core.fx.methodbased.MethodStmtCount;
-import core.fx.methodbased.ProportionOfRelevantStmts;
-import core.fx.methodstmtbased.ProportionOfRelevantStmtsBeforeStmt;
-import core.fx.methodstmtbased.ProportionOfVisitedMethodBeforeStmt;
-import core.fx.methodstmtbased.StmtDepthProportion;
-import core.fx.methodvarbased.ProportionOfVisitedMethod;
-import core.fx.methodvarbased.RelatedTypesCount;
-import core.fx.methodvarbased.TypeHierarchySize;
+import core.fx.methodbased.PropOfInvokeAssign;
+import core.fx.methodbased.methodstmtbased.*;
+import core.fx.methodbased.methodvarbased.MethodVarFEU;
+import core.fx.methodbased.methodvarbased.PropOfRelatedInvoke;
+import core.fx.methodbased.methodvarbased.RelatedTypesCount;
+import core.fx.variablebased.TypeHierarchySize;
 import java.util.HashMap;
 import java.util.Map;
 import soot.SootMethod;
 import soot.Value;
 import soot.jimple.Stmt;
-import soot.jimple.internal.JReturnStmt;
-import soot.jimple.internal.JReturnVoidStmt;
 
 public class FeatureExtractor {
 
   private static FeatureExtractor INSTANCE;
 
+  private MethodStmtFEU<Boolean> isReturnStmt = new IsReturnStmt();
   private MethodFEU<Integer> stmtCount = new MethodStmtCount();
   private MethodVarFEU<Integer> relatedTypesCount = new RelatedTypesCount();
-  private MethodVarFEU<Integer> typeHierarchySize = new TypeHierarchySize();
-  private MethodVarFEU<Double> propOfVisitedMethodTAS = new ProportionOfVisitedMethod();
-  private MethodFEU<Double> maxPropOfTAS = new ProportionOfRelevantStmts();
-  private MethodStmtFEU<Double> stmtDepth = new StmtDepthProportion();
+  private VariableFEU<Integer> typeHierarchySize = new TypeHierarchySize();
+  private MethodVarFEU<Double> propOfVisitedMethodTAS = new PropOfRelatedInvoke();
+  private MethodFEU<Double> maxPropOfTAS = new PropOfInvokeAssign();
+  private MethodStmtFEU<Double> stmtDepth = new StmtDepth();
   private MethodFEU<Integer> allocCount = new AllocationCount();
-  private MethodStmtFEU<Double> maxPropOfAAS = new ProportionOfRelevantStmtsBeforeStmt();
-  private MethodStmtFEU<Double> propOfVisitedMethodAAS = new ProportionOfVisitedMethodBeforeStmt();
+  private MethodStmtFEU<Double> maxPropOfAAS = new PropOfRelevantStmtBeforeStmt();
+  private MethodStmtFEU<Double> propOfVisitedMethodAAS = new PropOfInvokeBeforeStmt();
 
   public static FeatureExtractor getInstance() {
     if (INSTANCE == null) {
@@ -39,30 +37,19 @@ public class FeatureExtractor {
     return INSTANCE;
   }
 
-  // todo: change key name later
   public Map<String, Float> extract(SootMethod method, Value value, Stmt stmt) {
     Map<String, Float> featuresMap = new HashMap<>();
-    // HasSameReturnedType
-    if (method.getReturnType() != null && method.getReturnType().equals(value.getType())) {
-      featuresMap.put("x1", 1.0f);
-    } else {
-      featuresMap.put("x1", 0.0f);
-    }
-    // IsTailStmt
-    if (stmt instanceof JReturnStmt || stmt instanceof JReturnVoidStmt) {
-      featuresMap.put("x2", 1.0f);
-    } else {
-      featuresMap.put("x2", 0.0f);
-    }
-    featuresMap.put("x3", stmtCount.extract(method).getValue().floatValue());
-    featuresMap.put("x4", relatedTypesCount.extract(method, value).getValue().floatValue());
-    featuresMap.put("x5", typeHierarchySize.extract(method, value).getValue().floatValue());
-    featuresMap.put("x6", propOfVisitedMethodTAS.extract(method, value).getValue().floatValue());
-    featuresMap.put("x7", maxPropOfTAS.extract(method).getValue().floatValue());
-    featuresMap.put("x8", stmtDepth.extract(method, stmt).getValue().floatValue());
-    featuresMap.put("x9", allocCount.extract(method).getValue().floatValue());
-    featuresMap.put("x10", maxPropOfAAS.extract(method, stmt).getValue().floatValue());
-    featuresMap.put("x11", propOfVisitedMethodAAS.extract(method, stmt).getValue().floatValue());
+    featuresMap.put(
+        "x1", isReturnStmt.extract(method, stmt).getValue().booleanValue() ? 1.0f : 0.0f);
+    featuresMap.put("x2", stmtCount.extract(method).getValue().floatValue());
+    featuresMap.put("x3", relatedTypesCount.extract(method, value).getValue().floatValue());
+    featuresMap.put("x4", typeHierarchySize.extract(value).getValue().floatValue());
+    featuresMap.put("x5", propOfVisitedMethodTAS.extract(method, value).getValue().floatValue());
+    featuresMap.put("x6", maxPropOfTAS.extract(method).getValue().floatValue());
+    featuresMap.put("x7", stmtDepth.extract(method, stmt).getValue().floatValue());
+    featuresMap.put("x8", allocCount.extract(method).getValue().floatValue());
+    featuresMap.put("x9", maxPropOfAAS.extract(method, stmt).getValue().floatValue());
+    featuresMap.put("x10", propOfVisitedMethodAAS.extract(method, stmt).getValue().floatValue());
     return featuresMap;
   }
 }
